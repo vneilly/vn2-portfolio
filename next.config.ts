103a2ef7aa6ffa790b1b143e7cdecbf54ff70dd7
Vernon Neilly <vernonneilly@Vernons-MBP.lan>
2025-02-02 11:36:18 -0800

    Initial project config and setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  optimizeFonts: true,
 };
 
 export default nextConfig;
